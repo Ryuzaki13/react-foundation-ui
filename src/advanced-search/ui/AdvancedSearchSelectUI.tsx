0c99df2d1d9e59a8f1374f21f7e3b0f785e48f73
Ryuzaki13 <ryuzaki_dk_rm_guitar@mail.ru>
2026-08-05 18:13:54 +0500

    fix: selected token normal font weight

--- src/advanced-search/ui/AdvancedSearchSelectUI.tsx
+++ src/advanced-search/ui/AdvancedSearchSelectUI.tsx
@@ -53,7 +53,7 @@ export const AdvancedSearchSelectUI = ({
 
 				<div className={inputStyles.controlEndAdornment}>
 					<div className={uiStyles.uiToggleButtonContainer}>
-						<div className="flexEllipsis fontSizeSm fontLight" style={{ maxWidth: "100%" }}>
+						<div className="flexEllipsis fontSizeSm" style={{ maxWidth: "100%" }}>
 							{token}
 						</div>
 
